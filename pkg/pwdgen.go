a053f4e672ce2011a741b2cbeb1d2fff280c9c8b
liuquanneng <liuqn4@midea.com>
2023-03-09 15:45:28 +0800

    update

--- pkg/pwdgen.go
+++ pkg/pwdgen.go
@@ -3,7 +3,7 @@ package pkg
 import (
 	"bytes"
 	"fmt"
-	"github.com/ultima-liu/password-generator/internal"
+	 "github.com/ultima-liu/password-generator/test"
 )
 
 type PasswordGenerator struct {
@@ -19,8 +19,7 @@ func (pg *PasswordGenerator) DigitOnly(length int) string{
 	var buf bytes.Buffer
 	digits := "0123456789"
 	for l := 0; l < length; l++ {
-
-		buf.WriteByte(digits[internal.RandNumber(10)])
+		buf.WriteByte(digits[test.RandNumber(10)])
 	}
 	return buf.String()
 }
@@ -28,7 +27,7 @@ func (pg *PasswordGenerator) CharOnly(length int) string{
 	var buf bytes.Buffer
 	digits := "abcdefghijklmnopqrstuvwxyz"
 	for l := 0; l < length; l++ {
-		buf.WriteByte(digits[internal.RandNumber(26)])
+		buf.WriteByte(digits[test.RandNumber(26)])
 	}
 	return buf.String()
 }
